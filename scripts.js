ff160f24035069f2fd39eaeb3c6c7542cf3838f3
Heather <80366350+heathjackson@users.noreply.github.com>
2023-03-07 23:00:57 -0500

    added eslint

--- scripts.js
+++ scripts.js
@@ -1,47 +1,46 @@
-const bookCards = document.getElementById("book-cards");
-const addNewBook = document.getElementById("newBookButton");
-const form = document.getElementById("add-form");
-const submitButton = document.getElementById("submit-button");
+const bookCards = document.getElementById('book-cards');
+const addNewBook = document.getElementById('newBookButton');
+const form = document.getElementById('add-form');
+const submitButton = document.getElementById('submit-button');
 const radioButtons = document.querySelectorAll('input[name="read"]');
+let read = '';
 
-
-let myLibrary = [
+const myLibrary = [
   {
-  title: "Brave New World",
-  author: "Aldous Huxley",
-  pages: "300",
-  read: "yes",
-},
+    title: 'Brave New World',
+    author: 'Aldous Huxley',
+    pages: '300',
+    read: 'yes',
+  },
   {
-  title: "As I Lay Dying",
-  author: "William Faulkner",
-  pages: "250",
-  read: "no",
-},
+    title: 'As I Lay Dying',
+    author: 'William Faulkner',
+    pages: '250',
+    read: 'no',
+  },
   {
-  title: "The Hunger Games",
-  author: "Suzanne Collins",
-  pages: "400",
-  read: "yes",
-},
-]
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    pages: '400',
+    read: 'yes',
+  },
+];
 
-
-
+// eslint-disable-next-line no-shadow
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-submitButton.addEventListener("click", function(event) {
+submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  form.style.visibility = "hidden";
+  form.style.visibility = 'hidden';
 
-  let title = document.getElementById("book-title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
+  const title = document.getElementById('book-title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
 
   for (const radioButton of radioButtons) {
     if (radioButton.checked) {
@@ -49,42 +48,40 @@ submitButton.addEventListener("click", function(event) {
     }
   }
 
-  let book = new Book(title, author, pages, read);
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 
-  //part of booksInLibrary()
-  let myArray = Object.values(book);
+  // part of booksInLibrary()
+  const myArray = Object.values(book);
 
-  let eachBookCard = document.createElement("div");
-  eachBookCard.classList.add("bookCardClass")
+  const eachBookCard = document.createElement('div');
+  eachBookCard.classList.add('bookCardClass');
   bookCards.appendChild(eachBookCard);
   eachBookCard.innerHTML = myArray;
 
-  let removeButton = document.createElement("button");
-  removeButton.classList.add("del");
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('del');
   eachBookCard.appendChild(removeButton);
-  removeButton.innerHTML = "Delete";
+  removeButton.innerHTML = 'Delete';
 
-  removeButton.addEventListener("click", function() {
-    this.closest(".bookCardClass").remove();
+  removeButton.addEventListener('click', function () {
+    this.closest('.bookCardClass').remove();
   });
 
   document.forms[0].reset();
 });
 
 function booksInLibrary() {
-  for (let book of myLibrary) {
-    let myArray = Object.values(book);
-    let eachBookCard = document.createElement("div");
+  for (const book of myLibrary) {
+    const myArray = Object.values(book);
+    const eachBookCard = document.createElement('div');
     bookCards.appendChild(eachBookCard);
     eachBookCard.innerHTML = myArray;
   }
 }
 
-
-
 booksInLibrary();
 
-addNewBook.addEventListener("click", function() {
-  form.style.visibility = "visible";
+addNewBook.addEventListener('click', () => {
+  form.style.visibility = 'visible';
 });
